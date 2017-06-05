b8cdb08142dc082e251de6273122d287c20a65ba
SirLeigh <contactsirleigh@gmail.com>
2017-06-05 18:47:13 -0400

    stream

---
+++ settings-and-images/settings.js
@@ -0,0 +1,18 @@
+var settings = {
+    "options": {
+        "pauseTime": "6",
+        "inbetweenPauseTime": "10"
+    },
+    "colors": {
+        "iconBoxColor": "rgba(0, 102, 153, 1)",
+        "textBoxColor": "rgba(0, 56, 83, 1)",
+        "iconColor": "rgba(255, 255, 255, 1)",
+        "fontColor": "rgba(255, 255, 255, 1)"
+    },
+    "fonts": {
+        "primaryFont": "Times New Roman",
+        "fontWeight": "600",
+        "fontSize": "40",
+        "textYOffset": "0"
+    }
+};
